63475d9851a28bb7aca6a0dd4521d33787c1e6d9
Edwin Agbenyega <eddybanks03@gmail.com>
2016-11-04 15:31:33 -0600

    changes to homepage

--- app/models/assessment_type.rb
+++ app/models/assessment_type.rb
@@ -1,6 +1,10 @@
 class AssessmentType < ApplicationRecord
   has_many    :assessment_subtypes
   belongs_to  :year
-  scope :ar, -> { where (rp_flag: true )}
-  scope :ap, -> { where (rp_flag: false )}
+  scope :ar, -> { where(rp_flag: true) }
+  scope :ap, -> { where(rp_flag: false) }
+
+  # def self.ar
+  #   where(rp_flag: true)
+  # end
 end
